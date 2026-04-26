6b11503a37caf6c54a15bb6a9b237667ad7e66f9
Top-Slayer <dragontops55@gmail.com>
2026-04-27 04:43:29 +0700

    add: using uv as packages manager instead of pip

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-python -m compileall src # check compile time
-python -m uvicorn main:app --reload --app-dir src
+if ! command -v uv > /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
+
+uv sync
+uv run python -m compileall src # check compile time
+uv run python -m uvicorn main:app --reload --app-dir src
